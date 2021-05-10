a1a1a7bd180abe7d93adf053b45f419c5d490f91
Miguel Ponce Local <miguel_p1194@hotmail.com>
2021-05-09 23:30:01 -0500

    Se agregan todos los archivos de la pagina web

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+/* id del enlace que despliega el menú */
+var lanzador = "#enlace-menu";
+
+/* id del menú que será desplegado */
+var desplegable = "#menu";
+
+/* clase (sin el punto) que muestra el menú */
+var despliegaClase = "menu-desplegado";	
+	
+/* A partir de aquí, puedes dejar el código tal cual */
+	
+/* declaramos las funciones */
+function nav(){
+	var lanz = document.querySelector(lanzador);	
+	lanz.addEventListener("click", despliegaMenu, false);
+}
+
+function despliegaMenu(e){
+	e.preventDefault();
+	var despl = document.querySelector(desplegable);
+	despl.classList.toggle(despliegaClase);
+}
+
+/* ejecutamos la función principal */
+nav();
